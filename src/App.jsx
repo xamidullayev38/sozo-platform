c5125411d0b5cbebe24c28d9be5586b3ca81900b
xamidullayev38 <xamidullayevsaidkamol@gmail.com>
2026-02-05 20:30:02 +0500

    start

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Button from '@mui/material/Button';
+
+function App() {
+  return (
+    <div style={{ padding: 40 }}>
+      <Button variant="contained">Salom MUI 🚀</Button>
+    </div>
+  );
+}
+
+export default App;
